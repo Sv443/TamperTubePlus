badb89a2baddd94387c542f05005f75a45cf4be6
Sv443 <38158426+Sv443@users.noreply.github.com>
2018-05-06 00:20:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,59 @@
+// ==UserScript==
+// @name         TamperTubePlus
+// @namespace    https://github.com/Sv443/TamperTubePlus
+// @version      0.1
+// @description  New YouTube features and general improvements
+// @author       Sv443
+// @match        *://www.youtube.com/*
+// @grant        none
+// ==/UserScript==
 
- 
+var disable_polymer_design = true; // disables the new ugly polymer design if set to true
+
+
+
+if(disable_polymer_design = true){
+    // this script is not made by me but by /u/ndogw and davidbailey95 (https://github.com/davidbailey95)
+    function changeUrl(url, always) {
+        if (url.indexOf("disable_polymer") === -1) {
+            if (url.indexOf("?") > 0) {
+                url += "&";
+            } else {
+                url += "?";
+            }
+            url += "disable_polymer=1";
+            window.location.href = url;
+        }
+        if (always) {
+            window.location.href = url;
+        }
+    }
+
+    var url = window.location.href;
+    changeUrl(url);
+
+    document.addEventListener('DOMContentLoaded', function() {
+        // from https://stackoverflow.com/a/12552017/4247209
+        document.body.onclick = function(e){
+            e = e || event;
+            var from = findParent('a',e.target || e.srcElement);
+            if (from) {
+                var url = from.href;
+                if (!(url.match("/embed/") || url === location.href)) {
+                    changeUrl(url, true);
+                    return false;
+                }
+            }
+        };
+        //find first parent with tagName [tagname]
+        function findParent(tagname,el){
+            while (el){
+                if ((el.nodeName || el.tagName).toLowerCase()===tagname.toLowerCase()){
+                    return el;
+                }
+                el = el.parentNode;
+            }
+            return null;
+        }
+    });
+}
