9c5c823712d4ee1e09d7f919db99ef20ce3319ba
Sv443 <38158426+Sv443@users.noreply.github.com>
2018-05-06 03:30:55 +0200

    Update tmscript.js

--- tmscript.js
+++ tmscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TamperTubePlus
 // @namespace    https://github.com/Sv443/TamperTubePlus
-// @version      0.0.2
+// @version      0.0.3
 // @description  New YouTube features and general improvements
 // @author       Sv443
 // @match        *://www.youtube.com/*
@@ -14,7 +14,7 @@
 var log_to_console = true; // log some debug info to the javascript console
 var disable_polymer_design = true; // disables the new ugly polymer design if set to true
 //var quick_bookmark_hotkey = 120; // hotkey for quick bookmark (default key: F9 (120)), to look up key codes go to this website: https://zeamedia.com/helper/javascript-key-codes-char-codes.php
-
+var download_hotkey = 119; // hotkey for quick video download (default key: F8 (119)), to look up key codes go to this website: https://zeamedia.com/helper/javascript-key-codes-char-codes.php
 
 
 
@@ -29,6 +29,9 @@ var disable_polymer_design = true; // disables the new ugly polymer design if se
 var URLhost = window.location.host;
 var URLpath = window.location.pathname;
 var curURL = URLhost + "" + URLpath;
+
+var queryString = window.location.search;
+queryString = queryString.substring(1);
 
 if(log_to_console = true){console.log("TamperMonkey:");}
 
@@ -104,3 +107,22 @@ document.addEventListener('keyup', function(e){
     }
 });
 */
+
+document.addEventListener('keyup', function(e){
+    if(e.keyCode == download_hotkey) {
+        openc2mp3();
+    }
+});
+
+function openc2mp3() {
+    var dl_format = prompt("Download video - choose format\nAvailable Options: mp3,m4a,aac,flac,ogg,wma,mp4,avi,wmv,3gp");
+    if(dl_format == "mp3" || dl_format == "m4a" || dl_format == "aac" || dl_format == "flac" || dl_format == "ogg" || dl_format == "wma" || dl_format == "mp4" || dl_format == "avi" || dl_format == "wmv" || dl_format == "3gp"){
+        window.open('http://convert2mp3.net/addon_call.php?format=' + dl_format + '&url=' + curURL + queryString);
+    }
+    else {
+        var confirmretry = confirm("Entered value does not match available file formats (mp3,m4a,aac,flac,ogg,wma,mp4,avi,wmv,3gp)\nTry again?");
+        if(confirmretry == true){
+            openc2mp3();
+        }
+    }
+}
