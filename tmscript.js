ef4b77a1ae6dcce3199df7abc4e89c9f130a8c1a
Sv443 <38158426+Sv443@users.noreply.github.com>
2018-05-06 04:03:02 +0200

    Update tmscript.js

--- tmscript.js
+++ tmscript.js
@@ -11,10 +11,12 @@
 // @updateURL    https://raw.githubusercontent.com/Sv443/TamperTubePlus/master/tmscript.js
 // ==/UserScript==
 
-var log_to_console = true; // log some debug info to the javascript console
-var disable_polymer_design = true; // disables the new ugly polymer design if set to true
-//var quick_bookmark_hotkey = 120; // hotkey for quick bookmark (default key: F9 (120)), to look up key codes go to this website: https://zeamedia.com/helper/javascript-key-codes-char-codes.php
-var download_hotkey = 119; // hotkey for quick video download (default key: F8 (119)), to look up key codes go to this website: https://zeamedia.com/helper/javascript-key-codes-char-codes.php
+
+// you can change these settings if you want to:
+    var log_to_console = false; // log some debug info to the javascript console
+    var disable_polymer_design = true; // disables the new ugly polymer design if set to true
+    //var quick_bookmark_hotkey = 120; // hotkey for quick bookmark (default key: F9 (120)), to look up key codes go to this website: https://zeamedia.com/helper/javascript-key-codes-char-codes.php
+    var download_hotkey = 119; // hotkey for quick video download (default key: F8 (119)), to look up key codes go to this website: https://zeamedia.com/helper/javascript-key-codes-char-codes.php
 
 
 
@@ -29,11 +31,11 @@ var download_hotkey = 119; // hotkey for quick video download (default key: F8 (
 var URLhost = window.location.host;
 var URLpath = window.location.pathname;
 var curURL = URLhost + "" + URLpath;
-
 var queryString = window.location.search;
 queryString = queryString.substring(1);
 
-if(log_to_console = true){console.log("TamperMonkey:");}
+
+if(log_to_console == true){console.log("--BEGIN TamperTubePlus");}
 
 if(disable_polymer_design = true){
     // this script is not made by me but by /u/ndogw and davidbailey95 (https://github.com/davidbailey95)
@@ -55,11 +57,11 @@ if(disable_polymer_design = true){
     var url = window.location.href;
     changeUrl(url);
 
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener("DOMContentLoaded", function() {
         // from https://stackoverflow.com/a/12552017/4247209
         document.body.onclick = function(e){
             e = e || event;
-            var from = findParent('a',e.target || e.srcElement);
+            var from = findParent("a",e.target || e.srcElement);
             if (from) {
                 var url = from.href;
                 if (!(url.match("/embed/") || url === location.href)) {
@@ -79,14 +81,13 @@ if(disable_polymer_design = true){
             return null;
         }
     });
-    if(log_to_console = true){console.log("    Disabled Polymer Design");}
+    if(log_to_console == true){console.log("    Disabled Polymer Design");}
 }
 
-
-/* DISABLED - doesn't work
-
-document.addEventListener('keyup', function(e){
+/* DISABLED
+document.addEventListener("keyup", function(e){
 	if(e.keyCode == quick_bookmark_hotkey) {
+        if(log_to_console == true){console.log("    registered keystroke: " + quick_bookmark_hotkey);}
         var title = prompt("Bookmark Title");
         var url = curURL;
         function bookmark(title, url) {
@@ -97,19 +98,19 @@ document.addEventListener('keyup', function(e){
                 window.sidebar.addPanel(title, url, "");
             }
             else if(window.opera && window.print) { // opera
-                var elem = document.createElement('a');
-                elem.setAttribute('href',url);
-                elem.setAttribute('title',title);
-                elem.setAttribute('rel','sidebar');
+                var elem = document.createElement("a");
+                elem.setAttribute("href",url);
+                elem.setAttribute("title",title);
+                elem.setAttribute("rel","sidebar");
                 elem.click(); // this.title=document.title;
             }
         }
     }
-});
-*/
+});*/
 
-document.addEventListener('keyup', function(e){
-    if(e.keyCode == download_hotkey) {
+document.addEventListener("keyup", function(f){
+    if(f.keyCode == download_hotkey) {
+        if(log_to_console == true){console.log("    registered keystroke: " + download_hotkey);}
         openc2mp3();
     }
 });
@@ -117,12 +118,25 @@ document.addEventListener('keyup', function(e){
 function openc2mp3() {
     var dl_format = prompt("Download video - choose format\nAvailable Options: mp3,m4a,aac,flac,ogg,wma,mp4,avi,wmv,3gp");
     if(dl_format == "mp3" || dl_format == "m4a" || dl_format == "aac" || dl_format == "flac" || dl_format == "ogg" || dl_format == "wma" || dl_format == "mp4" || dl_format == "avi" || dl_format == "wmv" || dl_format == "3gp"){
-        window.open('http://convert2mp3.net/addon_call.php?format=' + dl_format + '&url=' + curURL + queryString);
+        if(log_to_console == true){console.log("    download - entered correct file format: " + dl_format + ", downloading...");}
+        window.open("http://convert2mp3.net/addon_call.php?format=" + dl_format + "&url=" + curURL + queryString);
     }
     else {
-        var confirmretry = confirm("Entered value does not match available file formats (mp3,m4a,aac,flac,ogg,wma,mp4,avi,wmv,3gp)\nTry again?");
-        if(confirmretry == true){
-            openc2mp3();
+        if(dl_format == null || dl_format == "" || dl_format == "null"){
+            if(log_to_console == true){console.log("    download - cancelled operation");}
+            return null;
+        }
+        else{
+            if(log_to_console == true){console.log("    download - entered wrong file format: " + dl_format);}
+            var confirmretry = confirm("Entered value does not match available file formats (mp3,m4a,aac,flac,ogg,wma,mp4,avi,wmv,3gp)\nTry again?");
+            if(confirmretry == true){
+                openc2mp3();
+            }
+            else {
+                if(log_to_console == true){console.log("    download - cancelled operation after retrying");}
+            }
         }
     }
 }
+
+if(log_to_console == true){console.log("--END TamperTubePlus");}
